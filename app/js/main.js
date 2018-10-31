bf203ba69e3bca1c45953b9ee4a66a91976a86b2
Vitor Melo <vitormelo300@hotmail.com>
2018-10-31 15:36:32 -0300

    Ajuste de feriados

--- app/js/main.js
+++ app/js/main.js
@@ -10,17 +10,18 @@ $(document).ready(function () {
             // Fim exemplo
 
             // Janeiro
-            {'Date': new Date(2019, 0, 1), 'Modificator': 'important', 'Title': 'Dia mundial da paz', 'Month': months[0]},
+            {'Date': new Date(2019, 0, 1), 'Modificator': 'holiday', 'Title': 'Dia mundial da paz', 'Month': months[0]},
             {'Date': new Date(2019, 0, 20), 'Modificator': 'important', 'Title': 'Dia do farmacêutico', 'Month': months[0]},
             {'Date': new Date(2019, 0, 28), 'Modificator': 'important', 'Title': 'Dia Nacional de Combate e Prevenção da Hanseníase', 'Month': months[0]},
 
             // Fevereiro
+            {'Date': new Date(2019, 1, 1), 'Modificator': 'star', 'Title': 'Campanha de Gripe', 'Month': months[1]},
             {'Date': new Date(2019, 1, 4), 'Modificator': 'important', 'Title': 'Dia Mundial do Câncer', 'Month': months[1]},
             {'Date': new Date(2019, 1, 11), 'Modificator': 'important', 'Title': 'Dia Mundial do Enfermo', 'Month': months[1]},
             {'Date': new Date(2019, 1, 15), 'Modificator': 'important', 'Title': 'Dia Internacional de Luta contra o Câncer na Infância', 'Month': months[1]},
 
             // Março
-            {'Date': new Date(2019, 1, 5), 'Modificator': 'important', 'Title': 'Carnaval', 'Month': months[2]},
+            {'Date': new Date(2019, 2, 5), 'Modificator': 'holiday', 'Title': 'Carnaval', 'Month': months[2]},
             {'Date': new Date(2019, 2, 8), 'Modificator': 'important', 'Title': 'Dia Internacional da Mulher', 'Month': months[2]},
             {'Date': new Date(2019, 2, 20), 'Modificator': 'important', 'Title': 'Início do Outono', 'Month': months[2]},
             {'Date': new Date(2019, 2, 21), 'Modificator': 'important', 'Title': 'Dia Internacional da Síndrome de Down', 'Month': months[2]},
@@ -34,13 +35,14 @@ $(document).ready(function () {
             {'Date': new Date(2019, 3, 7), 'Modificator': 'important', 'Title': 'Dia Mundial da Saúde', 'Month': months[3]},
             {'Date': new Date(2019, 3, 11), 'Modificator': 'important', 'Title': 'Dia do Infectologista', 'Month': months[3]},
             {'Date': new Date(2019, 3, 13), 'Modificator': 'important', 'Title': 'Dia do Jovem', 'Month': months[3]},
-            {'Date': new Date(2019, 3, 21), 'Modificator': 'important', 'Title': 'Páscoa', 'Month': months[3]},
+            {'Date': new Date(2019, 3, 21), 'Modificator': 'holiday', 'Title': 'Páscoa', 'Month': months[3]},
             {'Date': new Date(2019, 3, 24), 'Modificator': 'star', 'Title': 'Dia mundial da meningite', 'Month': months[3]},
 
             //Maio
-            {'Date': new Date(2019, 4, 1), 'Modificator': 'important', 'Title': 'Dia do Trabalho/ Campanha Gripe', 'Month': months[4]},
+            {'Date': new Date(2019, 4, 1), 'Modificator': 'holiday', 'Title': 'Dia do Trabalho/ Campanha Gripe', 'Month': months[4]},
             {'Date': new Date(2019, 4, 8), 'Modificator': 'important', 'Title': 'Dia Internacional da Cruz Vermelha', 'Month': months[4]},
-            {'Date': new Date(2019, 4, 12), 'Modificator': 'important', 'Title': 'Dia da Enfermagem/ Dia das Mães', 'Month': months[4]},
+            {'Date': new Date(2019, 4, 12), 'Modificator': 'holiday', 'Title': 'Dia da Enfermagem/ Dia das Mães', 'Month': months[4]},
+            {'Date': new Date(2019, 4, 13), 'Modificator': 'important', 'Title': 'Dia do Combate Mundial a Influenza', 'Month': months[4]},
             {'Date': new Date(2019, 4, 15), 'Modificator': 'important', 'Title': 'Dia Internacional da Família', 'Month': months[4]},
             {'Date': new Date(2019, 4, 31), 'Modificator': 'important', 'Title': 'Dia Mundial sem Tabaco/ Término Campanha Gripe', 'Month': months[4]},
 
@@ -59,7 +61,6 @@ $(document).ready(function () {
             {'Date': new Date(2019, 6, 28), 'Modificator': 'star', 'Title': 'Dia Mundial de Luta Contra as Hepatites Virais', 'Month': months[6]},
 
             // Agosto
-            {'Date': new Date(2019, 7, 1), 'Modificator': 'important', 'Title': 'Campanha de catapora', 'Month': months[7]},
             {'Date': new Date(2019, 7, 5), 'Modificator': 'important', 'Title': 'Dia da Farmácia', 'Month': months[7]},
             {'Date': new Date(2019, 7, 11), 'Modificator': 'important', 'Title': 'Dias dos Pais', 'Month': months[7]},
             {'Date': new Date(2019, 7, 24), 'Modificator': 'important', 'Title': 'Dia da Infância', 'Month': months[7]},
@@ -69,22 +70,21 @@ $(document).ready(function () {
             {'Date': new Date(2019, 8, 5), 'Modificator': 'important', 'Title': 'Dia do Oficial de Farmácia', 'Month': months[8]},
             {'Date': new Date(2019, 8, 8), 'Modificator': 'important', 'Title': 'Dia Nacional de Luta por Medicamento', 'Month': months[8]},
             {'Date': new Date(2019, 8, 21), 'Modificator': 'important', 'Title': 'Dia da Luta Nacional das Pessoas com Deficiências', 'Month': months[8]},
-            {'Date': new Date(2019, 8, 23), 'Modificator': 'important', 'Title': 'Início da Primavera', 'Month': months[8]},
+            {'Date': new Date(2019, 8, 23), 'Modificator': 'star', 'Title': 'Início da Campanha de Catapora', 'Month': months[8]},
             {'Date': new Date(2019, 8, 27), 'Modificator': 'important', 'Title': 'Dia Nacional de Doação de Órgãos', 'Month': months[8]},
             {'Date': new Date(2019, 8, 29), 'Modificator': 'important', 'Title': 'Dia Mundial do Coração', 'Month': months[8]},
 
             // Outubro
-            {'Date': new Date(2019, 9, 1), 'Modificator': 'important', 'Title': 'Dia do Idoso', 'Month': months[9]},
+            {'Date': new Date(2019, 9, 1), 'Modificator': 'important', 'Title': 'Dia do Idoso/Dia Internacional da Terceira Idade', 'Month': months[9]},
             {'Date': new Date(2019, 9, 4), 'Modificator': 'important', 'Title': 'Dia Nacional do Agente Comunitário de Saúde', 'Month': months[9]},
             {'Date': new Date(2019, 9, 10), 'Modificator': 'important', 'Title': 'Dia Mundial da Saúde Mental', 'Month': months[9]},
             {'Date': new Date(2019, 9, 11), 'Modificator': 'important', 'Title': 'Dia Internacional da Menina', 'Month': months[9]},
-            {'Date': new Date(2019, 9, 12), 'Modificator': 'important', 'Title': 'Dia das Crianças', 'Month': months[9]},
+            {'Date': new Date(2019, 9, 12), 'Modificator': 'holiday', 'Title': 'Dia das Crianças', 'Month': months[9]},
             {'Date': new Date(2019, 9, 17), 'Modificator': 'important', 'Title': 'Dia Nacional da Vacinação', 'Month': months[9]},
             {'Date': new Date(2019, 9, 18), 'Modificator': 'important', 'Title': 'Dia do Médico', 'Month': months[9]},
             {'Date': new Date(2019, 9, 24), 'Modificator': 'important', 'Title': 'Dia Mundial de Combate à Poliomielite', 'Month': months[9]},
 
             // Novembro
-            {'Date': new Date(2019, 10, 5), 'Modificator': 'important', 'Title': 'Dia Nacional de Controle da Dengue (dia D)', 'Month': months[10]},
             {'Date': new Date(2019, 10, 12), 'Modificator': 'star', 'Title': 'Dia mundial do combate a pneumonia', 'Month': months[10]},
             {'Date': new Date(2019, 10, 14), 'Modificator': 'important', 'Title': 'Dia mundial do Diabetes', 'Month': months[10]},
             {'Date': new Date(2019, 10, 20), 'Modificator': 'important', 'Title': 'Dia do Biomédico', 'Month': months[10]},
@@ -96,8 +96,8 @@ $(document).ready(function () {
             {'Date': new Date(2019, 11, 3), 'Modificator': 'important', 'Title': 'Dia Internacional do Portador de Deficiência', 'Month': months[11]},
             {'Date': new Date(2019, 11, 9), 'Modificator': 'important', 'Title': 'Dia da Criança Especial', 'Month': months[11]},
             {'Date': new Date(2019, 11, 21), 'Modificator': 'important', 'Title': 'Início do Verão', 'Month': months[11]},
-            {'Date': new Date(2019, 11, 25), 'Modificator': 'important', 'Title': 'Natal', 'Month': months[11]},
-            {'Date': new Date(2019, 11, 31), 'Modificator': 'important', 'Title': 'Réveillon', 'Month': months[11]},
+            {'Date': new Date(2019, 11, 25), 'Modificator': 'holiday', 'Title': 'Natal', 'Month': months[11]},
+            {'Date': new Date(2019, 11, 31), 'Modificator': 'holiday', 'Title': 'Réveillon', 'Month': months[11]},
         ];
         var settings = {};
         var element = document.getElementById('caleandar');
