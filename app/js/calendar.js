db8d2c4a804af37eb5a53a81bd1fa94b4c69e979
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-05-15 16:05:10 -0300

    cleaning

---
+++ app/js/calendar.js
@@ -0,0 +1,15 @@
+var calendar = {
+    init: function(){
+        console.log('app starts');
+    },
+    mountCalendar: function(){},
+    drawCalendar: function(){},
+    changeMonth: function(){},
+    changeType: function(){},
+    mountSideBar: function(){}
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    calendar.init();
+});
---
+++ app/tests/calendar.test.js
@@ -0,0 +1,5 @@
+describe("when app starts", ()=>{
+  it('renders without crashing', () => {
+    document.createElement('div');
+  });
+});
